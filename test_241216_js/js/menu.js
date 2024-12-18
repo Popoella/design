20bc0f62220e652a4bdbfe6b2ada4b79d416d618
Popoella <rinjjang1004@naver.com>
2024-12-18 11:19:57 +0900

    241218 반응형메뉴

--- test_241216_js/js/menu.js
+++ test_241216_js/js/menu.js
@@ -1,5 +1,22 @@
 $(document).ready(function(){
 
+    let device_status //pc면 pc라는 값이 저장, 모바일이면 mobile 저장
+    let window_w 
+
+    function device_chk(){
+        window_w = $(window).width()
+        if(window_w > 640){//640보다 크면
+            device_status = 'pc'
+        }else{
+            device_status = 'mobile'
+        }
+        console.log('함수')
+    }
+
+    device_chk()//html 문서가 로딩되고 나면 1번 실행
+    $(window).resize(function(){//브라우저가 리사이즈 될 때마다 한번 실행
+        device_chk()
+    })
     /*
         pc버전에서 메뉴에 마우스를 오버하면
         1. header에 menu_pc 클래스를 추가해줘야 함
@@ -10,12 +27,20 @@ $(document).ready(function(){
     */
 
     $('header .gnb .gnb_wrap ul.depth1 > li').on('mouseenter', function(){
-        $('header').addClass('menu_pc')
-        $('header .gnb .gnb_wrap ul.depth1 > li').removeClass('active')
-        $(this).addClass('active')
+        if(device_status=='pc'){
+            $('header').addClass('menu_pc')
+            $('header .gnb .gnb_wrap ul.depth1 > li').removeClass('active')
+            $(this).addClass('active')
+        }
         
     })
-    $('header').on('mouseleave', function(){
+    $('header').on('mouseleave focusout', function(){
+        $('header').removeClass('menu_pc')
+        $('header .gnb .gnb_wrap ul.depth1 > li').removeClass('active')
+    })
+
+    /
+    $('header .gnb .gnb_wrap ul.depth1 > li:last-child ul.depth2 > li:last-child').on('focusout', function(){
         $('header').removeClass('menu_pc')
         $('header .gnb .gnb_wrap ul.depth1 > li').removeClass('active')
     })
